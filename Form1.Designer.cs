c35c6740a5caca88faf3f977da074346acc95280
Mario Cañola <macaola0@soy.sena.edu.co>
2025-08-05 09:38:47 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace form2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.click = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(365, 203);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 0;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(365, 231);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 1;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(377, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "resultado";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // click
+            // 
+            this.click.AccessibleDescription = "";
+            this.click.Location = new System.Drawing.Point(260, 27);
+            this.click.Name = "click";
+            this.click.Size = new System.Drawing.Size(337, 23);
+            this.click.TabIndex = 3;
+            this.click.Text = "Hola Mundo";
+            this.click.UseVisualStyleBackColor = true;
+            this.click.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División"});
+            this.comboBox1.Location = new System.Drawing.Point(355, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(892, 462);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.click);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button click;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // comboBox1.Items.AddRange(new string[] { "Suma", "Resta", "Multiplicación", "División" });
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hola", "titulo\n");
+            MessageBox.Show("mario cañola\n");
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            if (!int.TryParse(textBox7.Text, out a) || !int.TryParse(textBox8.Text, out b))
+            {
+                MessageBox.Show("Por favor, ingresa números válidos.");
+                return;
+            }
+
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecciona una operación.");
+                return;
+            }
+
+            string operacion = comboBox1.SelectedItem.ToString();
+            int resultado = 0;
+            string mensaje = "";
+
+            switch (operacion)
+            {
+                case "Suma":
+                    resultado = a + b;
+                    mensaje = "El resultado de la suma es: " + resultado;
+                    break;
+                case "Resta":
+                    resultado = a - b;
+                    mensaje = "El resultado de la resta es: " + resultado;
+                    break;
+                case "Multiplicación":
+                    resultado = a * b;
+                    mensaje = "El resultado de la multiplicación es: " + resultado;
+                    break;
+                case "División":
+                    if (b == 0)
+                    {
+                        MessageBox.Show("No se puede dividir por cero.");
+                        return;
+                    }
+                    resultado = a / b;
+                    mensaje = "El resultado de la división es: " + resultado;
+                    break;
+                default:
+                    MessageBox.Show("Operación no reconocida.");
+                    return;
+            }
+
+            MessageBox.Show(mensaje, "Resultado");
+        }
+
+    }
+
+}
